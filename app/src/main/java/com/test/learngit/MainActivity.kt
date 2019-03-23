e2a1039500dd32f90b281a0af9965679452162ca
Gian Sebastian <ganjasmara@cermati.com>
2019-03-23 17:37:23 +0700

    test 2

--- app/src/main/java/com/test/learngit/MainActivity.kt
+++ app/src/main/java/com/test/learngit/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.test.learngit
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text1.text = "haha"
     }
 }
